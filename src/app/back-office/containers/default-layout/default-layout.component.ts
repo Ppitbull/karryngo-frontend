66d913e6f7a70d5347f98e90d76e073e2c1694c8
Flambel SANOU <flambel55@gmail.com>
2021-02-06 16:47:30 +0100

    Notification modal added

--- src/app/back-office/containers/default-layout/default-layout.component.ts
+++ src/app/back-office/containers/default-layout/default-layout.component.ts
@@ -1,10 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 import { UserService } from '../../../shared/service/user/user.service';
 import { AuthService } from '../../../shared/service/auth/auth.service';
 import { navItems } from '../../../_nav';
 import { Message } from '../../../shared/entity/chat';
 import { ChatService } from '../../../shared/service/back-office/chat.service';
+import { Router } from '@angular/router';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+declare var $: any;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './default-layout.component.html',
@@ -18,13 +21,16 @@ export class DefaultLayoutComponent implements OnInit{
   public userName: string = this.userService.getUserInformations().field_firstname + ' ' + this.userService.getUserInformations().field_lastname;
   private balence: string = ' 000 ';
   private currency: string = 'XOF';
+  closeResult = '';
 
   today: number = Date.now();
 
   constructor(private userService: UserService,
    private authService: AuthService,
-   private chatService: ChatService) {
+   private chatService: ChatService,
+   private modalService: NgbModal) {
   }
+  
   ngOnInit(): void {
     this.chatService.listMessageUnreadSubject.subscribe((listMessage)=>this.unreadMessageList=listMessage);
   }
@@ -34,4 +40,41 @@ export class DefaultLayoutComponent implements OnInit{
   logOut() {
     this.authService.logOut();
   }
+
+  
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  
+  showNotification(from, align, colortype, icon, text) {
+
+    $.notify({
+      icon: icon,
+      message: text
+    }, {
+      type: colortype,
+      timer: 1000,
+      placement: {
+        from: from,
+        align: align
+      }
+    });
+  }
+
+
 }
