6cee6d19f54948cbed1977a7bcf1a6e1b46a10f3
Flambel SANOU <flambel55@gmail.com>
2021-02-06 18:31:52 +0100

    chat component interface updated

--- src/app/back-office/views/chat/chat.module.ts
+++ src/app/back-office/views/chat/chat.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
 
-import { ChatComponent } from './chat.component';
+import { ChatComponent } from './chat/chat.component';
 import { ChatRoutingModule } from './chat-routing.module';
 
 @NgModule({
